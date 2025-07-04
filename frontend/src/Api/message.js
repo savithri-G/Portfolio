543864060ee493a7e313bc5f578f0acde50f2c2c
savithri-G <121480789+savithri-G@users.noreply.github.com>
2025-07-04 10:44:40 +0530

    Update message.js

--- frontend/src/Api/message.js
+++ frontend/src/Api/message.js
@@ -1,10 +1,14 @@
 // sendMessage.js
+// frontend/src/Api/message.js
+
 export const sendMessage = async (form) => {
   try {
-    const res = await fetch("http://localhost/Savi/contact.php", {
+    const res = await fetch("https://portfolio-fqog.onrender.com/contact.php", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(form).toString(),
     });
 
     const result = await res.json();
@@ -13,5 +17,6 @@ export const sendMessage = async (form) => {
     return { success: false, error: "Connection failed" };
   }
 };
+
 // Usage example
 // import { sendMessage } from './path/to/sendMessage';
